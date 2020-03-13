cd3bb978aefd224958faa930d6299e525d225b58
JoshArendse <42963960+JoshArendse@users.noreply.github.com>
2020-03-13 10:15:01 +0200

    Refined
    added refinements to the game, improved math logic for calculating life bar health, added keyboard events to punch and some basic animations

--- js/app.js
+++ js/app.js
@@ -6,8 +6,26 @@ new Vue({
     },
 
     methods: {
-        punch: function(){
+        jab: function(){
             this.health -= 10;
+
+            if(this.health <= 0){
+                this.ended = true;
+            }
+
+        },
+
+        hook: function(){
+            this.health -= 20;
+
+            if(this.health <= 0){
+                this.ended = true;
+                this.health = 0;
+            }
+        },
+
+        ko: function(){
+            this.health = 0;
 
             if(this.health <= 0){
                 this.ended = true;
@@ -20,7 +38,18 @@ new Vue({
         }
     },
 
-    computed: {
-
-    },
 });
+
+$(document).keypress('keypress',function(e){
+    if(e.which == 106){
+        $('#bag').toggleClass('tilt');
+    }
+   
+    if(e.which == 104){
+        $('#bag').toggleClass('tilt2');
+    }
+
+    if(e.which == 107){
+        $('#bag').toggleClass('tilt3');
+    }
+})
